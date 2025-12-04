0bed6431eac08d3af3da455018e76f790fe76409
Vladimir Tsyganov <tsyganov.vv0228@gmail.com>
2025-12-04 15:26:48 +0700

    frontend on react(vite)

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Wellcome to MindSpark</h1>
+      <p>Your AI-powered assistant for creative thinking</p>
+
+      <div>
+        <h3>Get Started:</h3>
+          <ul>
+            <li><a href="">Authorization</a></li>
+            <li><a href="">Chat</a></li>
+          </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
