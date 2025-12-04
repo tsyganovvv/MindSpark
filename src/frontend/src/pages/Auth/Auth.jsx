9564659986b7ae757c7a573cc0df31f6e1760104
Vladimir Tsyganov <tsyganov.vv0228@gmail.com>
2025-12-05 00:29:49 +0700

    update google auth

--- src/frontend/src/pages/Auth/Auth.jsx
+++ src/frontend/src/pages/Auth/Auth.jsx
@@ -1,5 +1,14 @@
+
+
 export default function Auth(params){
+  function getGoogleAuthUrl() {
+    fetch('http://localhost:8000/auth/google/url', {method: 'POST'})
+      .then(response => window.location = response.url);
+  }
   return(
-    <h1>Auth</h1>
+    <>
+      <h1>Auth</h1>
+      <a href="#" onClick={ getGoogleAuthUrl() }>Google</a>
+    </>
   )
 }
