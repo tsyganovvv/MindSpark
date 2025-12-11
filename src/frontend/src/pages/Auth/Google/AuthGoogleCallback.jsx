2fa45d238a2f1484f17431b1ffce60274e09f3c8
Vladimir Tsyganov <tsyganov.vv0228@gmail.com>
2025-12-11 13:36:33 +0700

    google callback +

--- src/frontend/src/pages/Auth/Google/AuthGoogleCallback.jsx
+++ src/frontend/src/pages/Auth/Google/AuthGoogleCallback.jsx
@@ -1,7 +1,27 @@
+import { useEffect } from "react"
+import { useSearchParams } from 'react-router-dom';
+
 export default function AuthGoogleCallback(){
+    const [searchParams] = useSearchParams()
+
+    useEffect(()=> {
+        const code = searchParams.get('code');
+
+        if (code){
+            fetch('http://localhost:8000/auth/google/callback', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ 'code' : code })
+            }).catch(error => {
+                console.error('Error:', error)
+            });
+        }
+    }, [])
     return(
         <>
-            <h1>1</h1>
+            <h1>authorization...</h1>
         </>
     )
 }
